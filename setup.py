8a2656e8a78b06ad87e6feca587e6a7dadbd1653
Andrean Franc <andrean.franc@gmail.com>
2015-09-03 11:25:59 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+import librarian_setup
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+VERSION = librarian_setup.__version__
+
+setup(
+    name='librarian-setup',
+    version=VERSION,
+    license='BSD',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=[
+        'librarian_core',
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com/Outernet-Project/librarian-core.git#egg=librarian_core-0.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Applicaton',
+        'Framework :: Bottle',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
