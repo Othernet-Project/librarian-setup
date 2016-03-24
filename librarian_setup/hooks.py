fa0c679a54be9c3fa559e4c67367c0fd391300a2
Andrean Franc <andrean.franc@gmail.com>
2016-03-24 19:22:54 +0100

    Register language settings in hook

--- librarian_setup/hooks.py
+++ librarian_setup/hooks.py
@@ -1,5 +1,10 @@
 import importlib
 
+from bottle_utils.i18n import lazy_gettext as _
+
+from librarian_core.contrib.i18n.consts import LANGS
+from librarian_core.contrib.i18n.utils import set_default_locale
+
 from .setup import Setup, SetupWizard
 from .steps import setup_language, setup_language_form, is_language_invalid
 
@@ -10,6 +15,10 @@ def component_member_loaded(supervisor, member, config):
         importlib.import_module(mod_path)  # they autoregister themselves
     except ImportError:
         pass  # no setup wizard steps in this component
+
+
+def settings_saved(settings):
+    set_default_locale(settings['general.default_language'])
 
 
 def initialize(supervisor):
@@ -29,3 +38,20 @@ def initialize(supervisor):
                           method='POST',
                           index=1,
                           test=is_language_invalid)
+    # register option to change default language
+    default_language = supervisor.config.get('i18n.default_locale', 'en')
+    locales = supervisor.config.get('i18n.locales', ['en'])
+    ui_languages = [(code, name) for code, name in LANGS if code in locales]
+    help_text = _("Interface language that is initially selected for all "
+                  "users. Users can change it independently later.")
+    supervisor.exts.settings.add_group('general', _("General settings"))
+    supervisor.exts.settings.add_field(name='default_language',
+                                       group='general',
+                                       label=_("Default language"),
+                                       value_type='select',
+                                       help_text=help_text,
+                                       required=True,
+                                       default=default_language,
+                                       choices=ui_languages)
+    # register handler that sets default language when settings are saved
+    supervisor.exts.events.subscribe('SETTINGS_SAVED', settings_saved)
